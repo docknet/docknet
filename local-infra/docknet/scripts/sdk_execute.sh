1b43896b1aa1e83af67201684056787cec099f4c
6daniel32 <6daniel32@gmail.com>
2025-04-03 22:45:22 +0100

    feat: First version finished

--- local-infra/docknet/scripts/sdk_execute.sh
+++ local-infra/docknet/scripts/sdk_execute.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# Validate mandatory argument presence.
-if [ -z "$1" ]; then
-	echo $'Missing docknet command to execute!\n';
-	echo $'\nFor more information, please refer to the official documentation: https://learn.microsoft.com/en-us/dotnet/core/tools/';
-	exit 1;
-fi
-
 # Start the container if it's stopped or build
 # and start it if it doesn't exist.
 if [ ! "$(docker ps -q -f name=sdk)" ]; then
 	echo $'Starting SDK...\n';
-    docker compose up -d sdk;
+    if ! docker compose up -d sdk; then
+        echo "Error: Failed to start the sdk container."
+        exit 1;
+    fi
     echo $'\nSDK successfully built!\n';
+fi
+
+# When creating a new dotnet project, we need to ensure that all projects that 
+# allow the "--exclude-launch-settings" flag use it. This is the way to ensure
+# that no arbitrary port will be defined in the running profiles and 
+# "dotnet watch" will work.
+projects_which_can_exclude_launch_settings=(
+    "worker" "grpc" "blazor" "blazorwasm" "web" "mvc" "webapp" "webapi" "webapiaot"
+);
+
+if [ "$1" == 'new' ] && \
+[[ ! "$@" =~ "--exclude-launch-settings" ]]; then
+    for no_launch_settings_project in "${projects_which_can_exclude_launch_settings[@]}"; do
+        if [ "$2" == "$no_launch_settings_project" ]; then
+            docker exec -it sdk dotnet "$@" --exclude-launch-settings;
+            exit 0;
+        fi
+    done
 fi
 
 docker exec -it sdk dotnet "$@";
