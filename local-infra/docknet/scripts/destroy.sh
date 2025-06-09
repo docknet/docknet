7ffbc482bbab57f47bf8237971c3848cfdefa9ae
6daniel32 <6daniel32@gmail.com>
2025-06-09 20:43:10 +0100

    feat: Simplify project using dev containers

--- local-infra/docknet/scripts/destroy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activate the Bash extended globbing pattern.
-shopt -s extglob
-
-# Delete dotnet project
-rm -rf local-infra/docknet/usersecrets/*
-rm -rf .config
-rm -rf !("docker-compose.yml"|"docknet.sh"|"local-infra"|"README.md")
